37688ca794048e44ef06d1f66b90e3149982b0db
FedchenkoVyacheslav <60151556+FedchenkoVyacheslav@users.noreply.github.com>
2020-04-07 10:30:36 +0400

    ДЗ №3

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,21 +1,21 @@
-var modal = document.querySelector(".modal-bg_js");
-var buttonOpen = document.querySelector(".poster-button_js");
-var buttonClose = document.querySelector(".modal-close_js");
-var input = document.querySelector(".form-input_js");
+var modal = document.querySelector(".modal-bg__js");
+var buttonOpen = document.querySelector(".poster__button_js");
+var buttonClose = document.querySelector(".modal-window__close__js");
+var input = document.querySelector(".form-input__js");
 
 buttonOpen.addEventListener("click", function(){
-  modal.classList.remove("modal-bg_close");
+  modal.classList.remove("modal-bg__close");
   input.focus();
 });
 
 buttonClose.addEventListener("click", function(){
-  modal.classList.add("modal-bg_close");
+  modal.classList.add("modal-bg__close");
   buttonOpen.focus();
 });
 
 window.addEventListener("keydown", function(event){
-  if(!modal.classList.contains("modal-bg_close") && event.code === "Escape") {
-    modal.classList.add("modal-bg_close");
+  if(!modal.classList.contains("modal-bg__close") && event.code === "Escape") {
+    modal.classList.add("modal-bg__close");
     buttonOpen.focus();
   }
 });
