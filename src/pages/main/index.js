f4a50436377da520c848741a32996eaec7730f68
FedchenkoVyacheslav <60151556+FedchenkoVyacheslav@users.noreply.github.com>
2020-04-09 01:57:45 +0400

    ДЗ №3
    правки

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,7 @@
 var modal = document.querySelector(".modal-bg__js");
 var buttonOpen = document.querySelector(".poster__button_js");
-var buttonClose = document.querySelector(".modal-window__close__js");
-var input = document.querySelector(".form-input__js");
+var buttonClose = document.querySelector(".form__close__js");
+var input = document.querySelector(".form__input__js");
 
 buttonOpen.addEventListener("click", function(){
   modal.classList.remove("modal-bg__close");
