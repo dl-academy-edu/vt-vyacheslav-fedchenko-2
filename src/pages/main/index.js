483fcdc2c3de9f4ab4ca3f467b0e76334865c907
FedchenkoVyacheslav <60151556+FedchenkoVyacheslav@users.noreply.github.com>
2020-04-22 23:53:11 +0400

    ДЗ №6 и №7
    Сделал адаптивную верстку сайта с кадрированием и ретинизацией изображений

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -19,3 +19,16 @@ window.addEventListener("keydown", function(event){
     buttonOpen.focus();
   }
 });
+
+
+var buttonMobileHeader = document.querySelector(".header__mobile-menu_js");
+var buttonCloseMobileHeader = document.querySelector(".mobile-header__button-close_js");
+var mobileHeader = document.querySelector(".mobile-header");
+
+buttonMobileHeader.addEventListener("click", function(){
+  mobileHeader.classList.add("mobile-header_open");
+});
+
+buttonCloseMobileHeader.addEventListener("click", function(){
+  mobileHeader.classList.remove("mobile-header_open");
+});
