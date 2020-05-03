4dac7c4f2cf384bd561ce3eca04e35a58fd579a5
FedchenkoVyacheslav <60151556+FedchenkoVyacheslav@users.noreply.github.com>
2020-05-04 01:43:39 +0400

    ДЗ №9
    Внес правки по transition и мобильному header.  Добавил фоновую музыку. Сменил яндекс карты на google карты на главной странице. Добавил страницу Grid с реализацией адаптивности.

--- src/pages/grid/index.js
+++ src/pages/grid/index.js
@@ -1,6 +1,7 @@
 var buttonMobileHeader = document.querySelector(".header__mobile-menu_js");
 var buttonCloseMobileHeader = document.querySelector(".mobile-header__button-close_js");
 var mobileHeader = document.querySelector(".mobile-header");
+var mobileBG = document.querySelector(".header__mobile-bg");
 
 buttonMobileHeader.addEventListener("click", function(){
   mobileHeader.classList.add("mobile-header_open");
@@ -8,4 +9,12 @@ buttonMobileHeader.addEventListener("click", function(){
 
 buttonCloseMobileHeader.addEventListener("click", function(){
   mobileHeader.classList.remove("mobile-header_open");
+});
+
+buttonMobileHeader.addEventListener("click", function(){
+  mobileBG.classList.add("header__mobile-bg_open");
+});
+
+buttonCloseMobileHeader.addEventListener("click", function(){
+  mobileBG.classList.remove("header__mobile-bg_open");
 });
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -24,6 +24,7 @@ window.addEventListener("keydown", function(event){
 var buttonMobileHeader = document.querySelector(".header__mobile-menu_js");
 var buttonCloseMobileHeader = document.querySelector(".mobile-header__button-close_js");
 var mobileHeader = document.querySelector(".mobile-header");
+var mobileBG = document.querySelector(".header__mobile-bg");
 
 buttonMobileHeader.addEventListener("click", function(){
   mobileHeader.classList.add("mobile-header_open");
@@ -31,4 +32,12 @@ buttonMobileHeader.addEventListener("click", function(){
 
 buttonCloseMobileHeader.addEventListener("click", function(){
   mobileHeader.classList.remove("mobile-header_open");
+});
+
+buttonMobileHeader.addEventListener("click", function(){
+  mobileBG.classList.add("header__mobile-bg_open");
+});
+
+buttonCloseMobileHeader.addEventListener("click", function(){
+  mobileBG.classList.remove("header__mobile-bg_open");
 });
