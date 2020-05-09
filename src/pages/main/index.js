8d99669bd45ad75523c4d5048ab73a677d030fd9
FedchenkoVyacheslav <60151556+FedchenkoVyacheslav@users.noreply.github.com>
2020-05-09 10:43:00 +0400

    form fix

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,21 +1,21 @@
-var modal = document.querySelector(".modal-bg_js");
+var modal = document.querySelector(".modal_js");
 var buttonOpen = document.querySelector(".poster__button_js");
-var buttonClose = document.querySelector(".form__close_js");
+var buttonClose = document.querySelector(".modal__close_js");
 var input = document.querySelector(".form__input_js");
 
 buttonOpen.addEventListener("click", function(){
-  modal.classList.remove("modal-bg_close");
+  modal.classList.remove("modal_close");
   input.focus();
 });
 
 buttonClose.addEventListener("click", function(){
-  modal.classList.add("modal-bg_close");
+  modal.classList.add("modal_close");
   buttonOpen.focus();
 });
 
 window.addEventListener("keydown", function(event){
-  if(!modal.classList.contains("modal-bg_close") && event.code === "Escape") {
-    modal.classList.add("modal-bg_close");
+  if(!modal.classList.contains("modal_close") && event.code === "Escape") {
+    modal.classList.add("modal_close");
     buttonOpen.focus();
   }
 });
